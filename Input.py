e56ddf9b1591156bde0cbc3200545a3775063a37
Medix1075 <148248290+Medix1075@users.noreply.github.com>
2024-07-18 17:15:51 +0530

    Update Input.py

--- Input.py
+++ Input.py
@@ -4,11 +4,13 @@ from langchain.chains import ConversationChain
 from langchain.agents import load_tools, AgentType, initialize_agent
 from langchain.llms import GooglePalm
 from langchain.llms.base import BaseLLM
+import Serp.txt
+from Serp.txt import Serp_api_key, Google_api_key
 import os
-os.environ['SERPAPI_API_KEY'] = "e8d97f98bb34fd5b869c25a7c21bd49e1beec1d7d788882146cdb8fe9f7861a9"
+os.environ['SERPAPI_API_KEY'] = Serp_api_key
 
 
-llm = GooglePalm(google_api_key="AIzaSyAfufg_BdiAuEuBPr6BjlLYgopRmPuaSao", temperature=0.8)
+llm = GooglePalm(google_api_key=Google_api_key, temperature=0.8)
 
 
 tools = load_tools(['wikipedia', 'serpapi', 'llm-math'], llm=llm)
